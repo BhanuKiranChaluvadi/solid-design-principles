6f9ffe6b347972aa13a81fb2e29a55b5d6d48da4
BhanuKiranChaluvadi <nthdimensionalreality@gmail.com>
2020-04-29 22:48:29 +0200

    Added Shoe class

--- no_solid_design/card.py
+++ no_solid_design/card.py
@@ -13,6 +13,18 @@ class Card:
         elif 2 <= rank < 11: return (rank, rank)
         else: return (10, 10)
 
+class Shoe(Card):
+    def __init__(self, n):
+        super().__init__()
+        self.shoe = []
+        for _ in range(n): self.shoe.extend(self.cards)
+        random.shuffle(self.shoe)
+    def shuffle_burn(self, n=100):
+        random.shuffle(self.shoe)
+        self.shoe = self.shoe[n:]
+    def deal(self):
+        return self.shoe.pop()
+
 
 if __name__ == "__main__":
     import doctest
