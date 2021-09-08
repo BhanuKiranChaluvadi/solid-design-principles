e31f4e275c00d9ff8987cd29b39960f157e83ca3
bkch <bkch@universal-robots.com>
2021-09-08 14:21:50 +0200

    Added rapidJson as a submodule

---
+++ pluralSight/structural-adoptorToDecorator/Adaptor/src/MovieApiAdapter.cpp
@@ -0,0 +1,27 @@
+#include "MovieApiAdapter.h"
+#include <string>
+#include "rapidjson/document.h"
+#include <memory>
+
+#include "ErrorParsingMovieDataException.h"
+
+std::shared_ptr<MovieData> MovieApiAdapter::GetById(const std::string& movie_id)
+{
+	const auto json = api_.GetMovieDetails(movie_id);
+
+	rapidjson::Document jsonDocument;
+	jsonDocument.Parse(json.c_str());
+	if (!jsonDocument.IsObject())
+	{
+		throw ErrorParsingMovieDataException();
+	}
+
+	auto imdb_id = jsonDocument["imdb_id"].GetString();
+	auto title = jsonDocument["title"].GetString();
+	auto plot = jsonDocument["overview"].GetString();
+	auto length_min = jsonDocument["runtime"].GetUint();
+
+	auto result = std::make_shared<MovieData>(imdb_id, title, length_min, plot);
+
+	return result;
+}
